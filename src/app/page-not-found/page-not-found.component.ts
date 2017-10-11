23469ecd964ab48ca7129add661b280673496700
Ankita-Palekar <453ankitapalekar@gmail.com>
2017-10-12 00:42:35 +0530

    addded page not found and home componet

---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.css']
+})
+export class PageNotFoundComponent implements OnInit {
+  private image404= "./assets/images/404.jpg";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
